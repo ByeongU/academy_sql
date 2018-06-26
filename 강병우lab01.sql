a8060a05b2c61a97fbb12c7ba54c5d68fc6c41af
myquddn21@naver.com <>
2018-06-26 14:41:32 +0900

    sql 26일 실습과제 26)

---
+++ 강병우lab01.sql
@@ -0,0 +1,82 @@
+-- 실습1)
+SELECT e.EMPNO
+     , e.ENAME
+     , e.JOB
+     , e.SAL
+  FROM emp e
+ ORDER BY e.SAL DESC
+;
+
+-- 실습2)
+SELECT e.EMPNO
+     , e.ENAME
+     , e.HIREDATE
+  FROM emp e
+ ORDER BY e.HIREDATE
+;
+
+-- 실습3) 
+SELECT e.EMPNO
+     , e.EMPNO
+     , e.COMM
+  FROM emp e
+ ORDER BY e.COMM
+;
+
+-- 실습4) 
+SELECT e.EMPNO
+     , e.EMPNO
+     , e.COMM
+  FROM emp e
+ ORDER BY e.COMM DESC
+;
+
+-- 실습5
+SELECT e.EMPNO       사번
+     , e.ENAME       이름
+     , e.SAL         급여
+     , e.HIREDATE    입사일
+  FROM emp e
+;  
+
+-- 실습6) 
+
+-- 실습7) 
+
+-- 실습8)
+
+-- 실습9)
+
+-- 실습10)
+
+-- 실습11)
+
+-- 실습12)
+
+-- 실습13) 
+
+-- 실습14)
+
+-- 실습15)
+
+-- 실습16)
+
+-- 실습17)
+
+-- 실습18)
+
+-- 실습19)
+
+-- 실습20)
+
+-- 실습21)
+
+-- 실습22)
+
+-- 실습23)
+
+-- 실습24)
+
+-- 실습25)
+
+-- 실습26)
