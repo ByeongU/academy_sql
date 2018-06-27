e64fe9ad3b6d583c36050c2b606b8e6df88a9e43
강병우 <myquddn21@naver.com>
2018-06-27 17:43:41 +0900

    단일행 함수
    DECODE 까지

--- 강병우lab02.sql
+++ 강병우lab02.sql
@@ -68,6 +68,200 @@ SELECT e.EMPNO "사번"
 
 --------------------함수 실습
 
--- 실습1)
+-- 실습 1)
+SELECT INITCAP('ward') 
+  FROM dual
+; 
 
-  
+/*
+Ward
+*/
+
+-- 실습 2)
+SELECT LOWER('WARD') 
+  FROM dual
+; 
+
+/*
+ward
+*/
+
+-- 실습 3)
+SELECT UPPER('ward') 
+  FROM dual
+; 
+
+/*
+WARD
+*/
+
+-- 실습 4)
+SELECT LENGTH('korea')
+  FROM dual
+;
+
+SELECT LENGTHB('korea')
+  FROM dual
+;
+
+/*
+5
+*/
+
+-- 실습 5)
+SELECT LENGTH('강병우')
+  FROM dual
+;
+
+/*
+3
+*/
+
+SELECT LENGTHB('강병우')
+  FROM dual
+;
+
+/*
+9
+*/
+
+-- 실습 6)
+SELECT CONCAT('SQL', '배우기')
+  FROM dual
+;
+
+/*
+SQL배우기
+*/
+
+-- 실습 7)
+SELECT SUBSTR('SQL 배우기', 5, 2)
+  FROM dual
+;
+
+/*
+배우
+*/
+
+-- 실습 8)
+SELECT LPAD('SQL', 7, '$') 
+  FROM dual
+;
+
+/*
+$$$$SQL
+*/
+
+-- 실습 9)
+SELECT RPAD('SQL', 7, '$') 
+  FROM dual
+;
+
+/*
+SQL$$$$
+*/
+
+-- 실습 10)
+SELECT LTRIM('        sql 배우기  ')
+  FROM dual
+;
+
+/*
+sql 배우기  
+*/
+
+-- 실습 11)
+SELECT RTRIM('        sql 배우기  ')
+  FROM dual
+;
+
+/*
+        sql 배우기
+*/
+
+-- 실습 12)
+SELECT TRIM('        sql 배우기  ')
+  FROM dual
+;
+
+/*
+sql 배우기
+*/
+
+-- 실습 13)
+SELECT nvl(e.COMM, '0') 
+  FROM emp e
+;
+
+/*
+0
+300
+500
+0
+1400
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+*/
+
+-- 실습 14
+SELECT nvl2(e.SAL + e.COMM,'급여 + 커미션', '0')
+  FROM emp e
+;
+
+/*
+0
+급여 + 커미션
+급여 + 커미션
+0
+급여 + 커미션
+0
+0
+0
+급여 + 커미션
+0
+0
+0
+0
+0
+0
+*/
+
+-- 실습 15)
+SELECT e.EMPNO "사번"
+     , e.ENAME "이름"
+     , e.SAL   "월 급여"
+     , TO_CHAR (DECODE(e.JOB, 'CLERK' ,300
+                   , 'SALESMAN', 450
+                   , 'MANAGER', 600
+                   , 'ANALYST', 800
+                   , 'PRESIDENT', 1000),'$9,999') " 자기개발비"
+  FROM emp e
+;
+
+/*
+사번, 이름, 월 급여,  자기개발비
+--------------------------------
+7369	SMITH	800	       $300
+7499	ALLEN	1600	   $450
+7521	WARD	1250	   $450
+7566	JONES	2975	   $600
+7654	MARTIN	1250	   $450
+7698	BLAKE	2850	   $600
+7782	CLARK	2450	   $600
+7839	KING	5000	 $1,000
+7844	TURNER	1500	   $450
+7900	JAMES	950	       $300
+7902	FORD	3000	   $800
+7934	MILLER	1300	   $300
+9999	J_JUNE	500	       $300
+8888	J	    400	       $300
+7777	J%JUNES	300	       $300
+*/
